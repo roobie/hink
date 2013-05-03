b2f2b11384204e4c325d88566db3fe01f0e41b29
roobie <bjorn.roberg@gmail.com>
2013-05-03 18:35:09 +0200

    moar tests

--- test/test.js
+++ test/test.js
@@ -3,15 +3,35 @@ var ds = require("../datastructures");
 
 var Dictionary = ds.Dictionary;
 describe('Dictionary', function() {
-    var testKvp1 = new ds.KeyValuePair("testKey1", "testValue1");
-    var testKvp2 = new ds.KeyValuePair("testKey2", "testValue2");
-    var testKvp3 = new ds.KeyValuePair("testKey3", "testValue3");
-    var dict = new ds.Dictionary(testKvp1, testKvp2, testKvp3);
+    var testKvp1, testKvp2, testKvp3, dict
+
+    var init = function() {
+        testKvp1 = new ds.KeyValuePair("testKey1", "testValue1");
+        testKvp2 = new ds.KeyValuePair("testKey2", "testValue2");
+        testKvp3 = new ds.KeyValuePair("testKey3", "testValue3");
+        dict = new ds.Dictionary(testKvp1, testKvp2, testKvp3);
+    }
+
+    beforeEach(init);
+
+    describe('#ctor', function() {
+        it('should not accept object that are not instances of KeyValuePair', function() {
+            try {
+                new ds.Dictionary("")
+            } catch (e) {
+                assert.ok(e);
+                return;
+            }
+            assert.fail();
+        });
+    });
+
     describe('#get', function() {
         it('should return the KeyValuePair which corresponds to a certain key', function() {
             assert.equal(dict.get(testKvp1.key), testKvp1.value);
         });
     });
+
     describe('#set', function() {
         it('should set the value of the kvp to the new value', function() {
             var nv = "NEWVAL";
@@ -19,11 +39,21 @@ describe('Dictionary', function() {
             assert.equal(dict.get(testKvp2.key), nv);
         });
     });
+
     describe('#add', function() {
         it('should add the key value pair', function() {
             var nkvp = new ds.KeyValuePair("NEWKEY", "NEWVAL");
             dict.add(nkvp);
             assert.equal(dict.get(nkvp.key), nkvp.value);
+        });
+    });
+
+    describe('#remove', function() {
+        it('should remove the key value pair based on key', function() {
+            dict.remove(testKvp2.key);
+            setTimeout(function() {
+                assert.equal(dict.get(testKvp2.key), undefined);
+            }, 1);
         });
     });
 });
