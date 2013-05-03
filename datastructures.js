c621b6f63f4fcc80cb3a9711a7040afa343fde63
roobie <bjorn.roberg@gmail.com>
2013-05-03 11:15:21 +0200

    fixed indentation and compatibility issues

--- datastructures.js
+++ datastructures.js
@@ -1,8 +1,7 @@
-(function (window) {
+(function (root) {
     'use strict';
 
     var ds = {};
-    window.ds = ds;
 
     var kvp = ds.KeyValuePair = function(key, value) {
         this.key = key;
@@ -12,62 +11,70 @@
     var dict = ds.Dictionary = function() {
         var kvpairs = [];
 
-		this.get = function(key) {
-			var result = kvpairs.filter(function(kvp) {
-				if (kvp.key === key) {
-					return kvp.value;
-				}
-			});
-			if (result.length > 1) {
-				throw Error("More than one result in result set");
-			}
-			if (result.length == 1) {
-				return result[0].value;
-			}
-		}
+        this.get = function(key) {
+            var result = kvpairs.filter(function(kvp) {
+                if (kvp.key === key) {
+                    return kvp.value;
+                }
+            });
+            if (result.length > 1) {
+                throw Error("More than one result in result set");
+            }
+            if (result.length == 1) {
+                return result[0].value;
+            }
+        }
 
-		this.set = function(key, newValue) {
-			var result = kvpairs.filter(function(kvp) {
-				if (kvp.key === key) {
-					return kvp.value;
-				}
-			});
-			if (result.length > 1) {
-				throw Error("More than one result in result set");
-			}
-			if (result.length == 1) {
-				result[0].value = newValue;
-			}
-		}
+        this.set = function(key, newValue) {
+            var result = kvpairs.filter(function(kvp) {
+                if (kvp.key === key) {
+                    return kvp.value;
+                }
+            });
+            if (result.length > 1) {
+                throw Error("More than one result in result set");
+            }
+            if (result.length == 1) {
+                result[0].value = newValue;
+            }
+        }
 
-		this.count = function () {
-			return kvpairs.length;
-		}
+        this.count = function () {
+            return kvpairs.length;
+        }
 
-		this.keys = function() {
-			return kvpairs.map(function(e) {
-				return e.key;
-			});
-		}
+        this.keys = function() {
+            return kvpairs.map(function(e) {
+                return e.key;
+            });
+        }
 
-		this.values = function() {
-			return kvpairs.map(function(e) {
-				return e.value;
-			});
-		}
+        this.values = function() {
+            return kvpairs.map(function(e) {
+                return e.value;
+            });
+        }
 
         for (var i = 0, max = arguments.length; i < max; i++) {
             var item = arguments[i];
             if (item instanceof kvp) {
-				if (!this.get(item.key)) {
-					kvpairs.push(item);
-				} else {
-					throw Error("The key: `" + item.key + "` already exists in this Dictionary");
-				}
+                if (!this.get(item.key)) {
+                    kvpairs.push(item);
+                } else {
+                    throw Error("The key: `" + item.key + "` already exists in this Dictionary");
+                }
             } else {
                 throw TypeError("Parameter is not an instance of `asd.KeyValuePair`");
             }
         }
     }
 
-}(window));
+    if (root.require && typeof root.define == 'function') {
+        root.define([], ds);
+    } else {
+        root.ds = ds;
+    }
+
+}(global || window));
+
+
